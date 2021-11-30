440e444252d9e628ac173a03b716ca62ad4d58d1
SU\pavlo.malashnyak <pavlo.malashnyak@sigma.software>
2021-11-30 10:05:03 +0200

    INT-1: application backbone

---
+++ index.ts
@@ -0,0 +1,6 @@
+class Main {
+    public static main(): void {
+    }
+}
+
+Main.main();
---
+++ models/Airport.ts
@@ -0,0 +1,16 @@
+export class Airport {
+    constructor(public id: string, 
+        name: string, 
+        city: string,
+        country: string, 
+        IATA: string, 
+        ICAO: string, 
+        latitude: string, 
+        lontitude: string,
+        altitude: string, 
+        timezone: string, 
+        DST: string, 
+        tz: string, 
+        type: string, 
+        source: string) {}
+}
