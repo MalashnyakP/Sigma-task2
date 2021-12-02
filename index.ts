c9a34abe4008b6d3d6557c7a354daa11599a798d
SU\pavlo.malashnyak <pavlo.malashnyak@sigma.software>
2021-12-02 19:21:13 +0200

    INT-7: Build interactive command line interface for application

--- index.ts
+++ index.ts
@@ -4,79 +4,69 @@ import * as process from 'process';
 
 import { Airport } from './models/Airport';
 
-import { ISortAlgorithm } from './sort-algorithms/ISortAlgorithm';
 import { BubbleSort } from './sort-algorithms/bubble.js';
 import { QuickSort } from './sort-algorithms/quick.js';
 import { MergeSort } from './sort-algorithms/merge.js';
 import { HeapSort } from './sort-algorithms/heap.js';
 
-import { ISearchAlgorithm } from './search-algorithms/ISearchAlgorithm';
 import { BinarySearch } from './search-algorithms/binary.js';
 import { ExponentialSearch } from './search-algorithms/exponential.js';
 import { FrontNBackSearch } from './search-algorithms/frontAndBack.js';
 import { LinearSearch } from './search-algorithms/linear.js';
 
 import { FSService } from './services/fs.js';
+import { YargsService } from './services/yargs.js'
 
 class Main {
     public static main(): void {
-        let sortAlgorithm: ISortAlgorithm;
-        let searchAlgorithm: ISearchAlgorithm;
-
         const filePath = path.join(path.resolve('..'), 'dataset', 'airports.dat');
         const array =  FSService.read(filePath);
 
-        const fieldToSort = 'ee';
-        const order = 'asc';
+        const argv = YargsService.configureYargs();
 
-        if (this.isFieldOf(fieldToSort, array[0])) {
-            sortAlgorithm = new BubbleSort();
-            sortAlgorithm.sort(array, fieldToSort, order);
-            sortAlgorithm = new MergeSort();
-            sortAlgorithm.sort(array, fieldToSort, order);
-            sortAlgorithm = new QuickSort();
-            sortAlgorithm.sort(array, fieldToSort, order);
-            sortAlgorithm = new HeapSort();
-            sortAlgorithm.sort(array, fieldToSort, order);
-        } else {
-            console.log(`No ${fieldToSort} property in Airport.`);
+        switch (argv.action) {
+            case "sort": {
+                const field = argv.key;
+                const order = argv?.order;
+
+                new BubbleSort().sort(array, field, order);
+                new MergeSort().sort(array, field, order);
+                new QuickSort().sort(array, field, order);
+                new HeapSort().sort(array, field, order);
+
+                break;
+            }
+
+            case "search": {
+                const searchField = argv.key as keyof Airport;
+
+                if (!argv.search_value) {
+                    console.log("Please enter a value to search.");
+                    break;
+                }
+                const searchValue = argv.search_value;
+
+                new LinearSearch().search(array, searchValue, searchField);
+                new FrontNBackSearch().search(array, searchValue, searchField);
+
+                //Need sorted array for next 2 algorithms
+                const sortedArray = array.sort((a, b) => {
+                    const aValue = a[searchField];
+                    const bValue = b[searchField];
+                    if (aValue > bValue) return 1;
+                    if (aValue < bValue) return -1;
+                    return 0;
+                });
+
+                new BinarySearch().search(sortedArray, searchValue, searchField);
+                new ExponentialSearch().search(sortedArray, searchValue, searchField);
+
+                break;
+            }
         }
-
-        const searchField = 'country';
-        const searchValue = 'Ukraine';
-
-        if (this.isFieldOf(searchField, array[0])) {
-            searchAlgorithm = new LinearSearch();
-            searchAlgorithm.search(array, searchValue, searchField);
-            searchAlgorithm = new FrontNBackSearch();
-            searchAlgorithm.search(array, searchValue, searchField);
-
-            const sortedArray = array.sort((a, b) => {
-                const aValue = a[searchField];
-                const bValue = b[searchField];
-                if (aValue > bValue) return 1;
-                if (aValue < bValue) return -1;
-                return 0;
-            });
-
-            searchAlgorithm = new BinarySearch();
-            searchAlgorithm.search(sortedArray, searchValue, searchField);
-            searchAlgorithm = new ExponentialSearch();
-            searchAlgorithm.search(sortedArray, searchValue, searchField);
-        } else {
-            console.log(`No ${fieldToSort} property in Airport.`);
-        }
-    }
-
-    private static isFieldOf(key: string, airport: Airport): boolean {
-        if (!(key in airport)) {
-            return false;
-        }
-
-        return true;
     }
 }
 
-if (process.argv[1].split('.')[0] ===  fileURLToPath(import.meta.url).split('.')[0]) {
+if (process.argv[1].split('.')[0] === fileURLToPath(import.meta.url).split('.')[0]) {
     Main.main();
 }
