11072bafa37e05cfc91dfeee80097ea285968ffb
SU\pavlo.malashnyak <pavlo.malashnyak@sigma.software>
2021-12-02 10:43:34 +0200

    INT-4: refactoring

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
+import { fileURLToPath } from 'url';
+import * as process from 'process';
+
 class Main {
     public static main(): void {
     }
 }
 
-Main.main();
+if (process.argv[1].split('.')[0] ===  fileURLToPath(import.meta.url).split('.')[0]) {
+    Main.main();
+}
--- models/Airport.ts
+++ models/Airport.ts
@@ -1,16 +1,3 @@
-export class Airport {
-    constructor(public id: string, 
-        name: string, 
-        city: string,
-        country: string, 
-        IATA: string, 
-        ICAO: string, 
-        latitude: string, 
-        lontitude: string,
-        altitude: string, 
-        timezone: string, 
-        DST: string, 
-        tz: string, 
-        type: string, 
-        source: string) {}
+export interface Airport {
+    [key: string]: string,
 }
