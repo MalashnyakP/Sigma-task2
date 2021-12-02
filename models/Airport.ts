06970f297f372e4573d5e5024765030207b08f2f
SU\pavlo.malashnyak <pavlo.malashnyak@sigma.software>
2021-12-02 12:08:45 +0200

    INT-6: rebase fix

--- models/Airport.ts
+++ models/Airport.ts
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
 export interface Airport {
     [key: string]: string,
-=======
-export class Airport {
-    constructor(
-        public id: string, 
-        public name: string, 
-        public city: string,
-        public country: string, 
-        public IATA: string, 
-        public ICAO: string, 
-        public latitude: string, 
-        public lontitude: string,
-        public altitude: string, 
-        public timezone: string, 
-        public DST: string, 
-        public tz: string, 
-        public type: string, 
-        public source: string
-        ) {}
->>>>>>> be62008f5690d79c559449d6479ca0d6961a18a2
 }
--- sort.algorithms/ISortAlgorithm.ts
+++
@@ -1,5 +0,0 @@
-import { Airport } from '../models/Airport';
-
-export interface ISortAlgorithm {
-    sort(array: Airport[], field: string, order?: string): void;  
-}
